f398bd68bc8fa222ec2cf9810b0d1c89ea82fe16
joaufi <johnathan.a.fisher@gmail.com>
2016-12-15 16:27:44 -0500

    Create Products controller
    Team created Products controller to index and show a Product. Both
    actions are unauthenticated so that a user who is not logged in can
    still see all products available on the website.
    Next Steps: Setup Products routes

---
+++ app/controllers/products.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const controller = require('lib/wiring/controller');
+const models = require('app/models');
+const Product = models.product;
+
+const authenticate = require('./concerns/authenticate');
+
+const index = (req, res, next) => {
+  Product.find()
+    .then(products => res.json({ products }))
+    .catch(err => next(err));
+};
+
+const show = (req, res, next) => {
+  Product.findById(req.params.id)
+    .then(product => product ? res.json({ product }) : next())
+    .catch(err => next(err));
+};
+
+module.exports = controller({
+  index,
+  show,
+}, { before: [
+  { method: authenticate, except: ['index', 'show'] },
+], });
