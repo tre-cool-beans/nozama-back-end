a1dd91b25a594b25ccfdbf4d484b0c358fdd6c2c
Naki21 <Yianni21@gmail.com>
2016-12-16 13:02:14 -0500

    Add destroy crud script to pastorders
    We added the crud script to destroy past orders from our database.
    We tested it and it works. Only deletes if current user is validated.

---
+++ scripts/destroy-pastorder.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/pastorders"
+TOKEN="9f8ExMPTSoxbcr/B5svJeEGku9QaiYu/ZPgZjoZllhs=--IZDz4Cxf8wmEKzQKNLkW3LgIBPSoPg+z+xp9sy20VQA="
+ID="58541946f46e83ed4030d000"
+
+curl "${API}${URL_PATH}/${ID}" \
+  --include \
+  --request DELETE \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=$TOKEN"
+
+echo
