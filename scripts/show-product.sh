7f86133404372cf83ef2a6e437f4eaeeba550de1
Naki21 <Yianni21@gmail.com>
2016-12-16 09:45:47 -0500

    Add curl script for show 'product'
    We wrote a script for show on the product controller.
    We tested it and it worked.

--- scripts/show-product.sh
+++ scripts/show-product.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-API="http://localhost:4741"
-URL_PATH="/products"
+curl --include --request GET http://localhost:4741/products/5853f946ab9c61d9db935550
 
-curl "${API}${URL_PATH}${}" \
-  --include \
-  --request GET \
-  --header "Content-Type: application/json" \
-  --data '{
-      "product" : {
-        "title": "My TEST PRODUCT AAAAAAAAA",
-        "description": "A really nice mauve sweater",
-        "price": 3.50,
-        "sizes": ["S", "M", "L"],
-        "image_url": "https://images.luisaviaroma.com/Medium63I/ATE/017_c6a3c249-40a0-4f31-bdc8-a8ee7dd295b5.JPG"
-      }
-    }'
 
 echo
