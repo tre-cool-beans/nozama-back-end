4f38373c4cfb08726017bdefbb7fce6942b57e81
joaufi <johnathan.a.fisher@gmail.com>
2016-12-19 16:16:52 -0500

    Remove unneeded actions from PastOrders
    Removed all functions/actions that we are not going to need for
    PastOrders. Also gave authenticate and empty except array just to be
    careful, and removed a comment that was unneeded.

--- app/controllers/pastorders.js
+++ app/controllers/pastorders.js
@@ -13,12 +13,6 @@ const index = (req, res, next) => {
     .catch(err => next(err));
 };
 
-// const show = (req, res, next) => {
-//   PastOrder.findById(req.params.id)
-//     .then(pastorder => pastorder ? res.json({ pastorder }) : next())
-//     .catch(err => next(err));
-// };
-
 const create = (req, res, next) => {
   let pastorder = Object.assign(req.body.pastorder, {
     _owner: req.currentUser._id,
@@ -28,42 +22,9 @@ const create = (req, res, next) => {
     .catch(err => next(err));
 };
 
-const update = (req, res, next) => {
-  let search = { _id: req.params.id, _owner: req.currentUser._id };
-  PastOrder.findOne(search)
-    .then(pastorder => {
-      if (!pastorder) {
-        return next();
-      }
-
-      delete req.body._owner;  // disallow owner reassignment.
-      pastorder.comment = req.body.comment;
-      return pastorder.save();
-    })
-    .then(() => res.sendStatus(200))
-    .catch(err => next(err));
-};
-
-const destroy = (req, res, next) => {
-  let search = { _id: req.params.id, _owner: req.currentUser._id };
-  PastOrder.findOne(search)
-    .then(pastorder => {
-      if (!pastorder) {
-        return next();
-      }
-
-      return pastorder.remove()
-        .then(() => res.sendStatus(200));
-    })
-    .catch(err => next(err));
-};
-
 module.exports = controller({
   index,
-  // show,
   create,
-  update,
-  destroy,
 }, { before: [
-  { method: authenticate /*, except: ['index', 'show'] */},
+  { method: authenticate, except: [] },
 ], });
